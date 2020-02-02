544a6ee72427c70fb313f4d1c65dde94cd756cd1
Ehab Fawzy <ehab.fawzy@aurea.com>
2020-02-02 02:20:28 +0200

    add integration test

---
+++ src/test/java/com/demo/products/api/ProductControllerTest.java
@@ -0,0 +1,51 @@
+package com.demo.products.api;
+
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void whenGetProductsThenSuccess() throws Exception {
+        mockMvc.perform(get("/api/products"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(greaterThanOrEqualTo(4))));
+    }
+
+    @Test
+    public void whenGetProductByIdThenSuccess() throws Exception {
+        mockMvc.perform(get("/api/products/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name").value("HTC 10 EVO"));
+    }
+
+    @Test
+    public void whenCreateProductThenReturnCreatedProduct() throws Exception {
+        mockMvc.perform(post("/api/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{ \"name\": \"dummy-product\", \"currentPrice\": 10}"))
+                .andExpect(jsonPath("$.name").value("dummy-product"))
+                .andExpect(status().isCreated());
+    }
+
+}

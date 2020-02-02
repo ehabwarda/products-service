42d61a56d6f8c3d6f83587a7d6bea3ac95c7956d
Ehab Fawzy <ehab.fawzy@aurea.com>
2020-02-02 02:14:54 +0200

    change HTTP status code to CREATED for POST

--- src/main/java/com/demo/products/api/ProductController.java
+++ src/main/java/com/demo/products/api/ProductController.java
@@ -8,6 +8,8 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,9 +55,11 @@ public class ProductController {
     }
 
     @PostMapping
-    public ProductDTO createProduct(@RequestBody ProductDTO product) {
+    public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO product) {
         LOG.debug("create new product");
-        return productMapper.entityToApi(productService.saveProduct(productMapper.apiToEntity(product)));
+        final ProductDTO createdProduct = productMapper
+                .entityToApi(productService.saveProduct(productMapper.apiToEntity(product)));
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdProduct);
     }
 
 }
